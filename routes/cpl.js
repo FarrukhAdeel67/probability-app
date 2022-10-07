5412df33ef2df35b61f9c33961516738cf2c909a
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-10-07 20:02:46 +0500

    Update cpl.js

--- routes/cpl.js
+++ routes/cpl.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routerCpl = express.Router();
 routerCpl.use(express.json());
+
 const cplcontroller = require('../controllers/cpl');
 routerCpl.get('/',cplcontroller.getCpl);
 routerCpl.post("/", cplcontroller.postCpl);
